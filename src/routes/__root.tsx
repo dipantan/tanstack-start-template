65ba3ade057788df0d30fb3eeee2376306c01dcd
Dipantan Chakraborty <dipantan755@gmail.com>
2026-05-16 00:29:48 +0530

    initial commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,54 @@
+// src/routes/__root.tsx
+/// <reference types="vite/client" />
+import type { ReactNode } from "react";
+import {
+  Outlet,
+  createRootRoute,
+  HeadContent,
+  Scripts,
+} from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "TanStack Start Starter",
+      },
+    ],
+  }),
+  component: RootComponent,
+  notFoundComponent: NotFound,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function NotFound() {
+  return <p>Not Found</p>;
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
+}
